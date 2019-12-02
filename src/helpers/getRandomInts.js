fcd644b059c3c5c5fbd3a438049de947cfd23dc6
Giorgi Tsitsvidze <gtsitsvidze@host-80-241-248-70.customer.magticom.ge>
2019-12-02 15:20:29 +0400

    fix: numbers must be pirmes

--- src/helpers/getRandomInts.js
+++ src/helpers/getRandomInts.js
@@ -1,15 +1,22 @@
+const isPrime = num => {
+  for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+    if(num % i === 0) return false
+  return num > 1
+}
+
 const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
-const getRandomInts = (num) =>{
-  var ints = []
+const getRandomInts = num =>{
+  const ints = []
   while (ints.length < num) {
-    var randNum = getRandomInt(1, 50)
-    if(ints.indexOf(randNum) === -1){
+    const randNum = getRandomInt(1, 50)
+    if(ints.indexOf(randNum) === -1 && isPrime(randNum)){
       ints.push(randNum, randNum)
     }
   }
+
   return ints.sort(() => Math.random() - 0.5)
 }
 
